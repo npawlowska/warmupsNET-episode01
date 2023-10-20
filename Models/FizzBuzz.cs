76de0deb394b1165c0b358ff0d48dc23f21a3a64
Iza Stolarska <i.stolarska@pb.edu.pl>
2023-10-20 16:26:59 +0200

    FizzBuzz z wykorzystaniem RazorPages

---
+++ Models/FizzBuzz.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace warmupsNET_episode01.Models
+{
+    public class FizzBuzz
+    {
+        [Display(Name = "Wpisz liczbę")]
+        [Range(1, 100, ErrorMessage = "Oczekiwana wartość {0} z zakredu {1} i {2}.")]
+        [Required(ErrorMessage="Pole jest obowiązkowe")]
+        public int? Number { get; set; }
+        public string Check()
+        {
+            string OutputString = "";
+
+            if (Number % 3 == 0)
+            {
+                OutputString += "Fizz";
+            }
+            if (Number % 5 == 0)
+            {
+                OutputString += "Buzz";
+            }
+            if (OutputString == "")
+                return Number.ToString();
+            return OutputString;
+        }
+
+    }
+}
+
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using warmupsNET_episode01.Models;
+
+namespace warmupsNET_episode01.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public FizzBuzz FizzBuzz { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                ViewData["Result"] = FizzBuzz.Check(); 
+                //można też użyć TempData["Result"]
+            }
+            return Page();
+
+        }
+    }
+}
